15fa9130a7d2cb386753347efa1648e2643c525c
Square596 <pshenitsyn.as@phystech.edu>
2022-09-23 15:33:51 +0300

    Task2 2.0

--- 1.sh
+++ 1.sh
@@ -4,11 +4,11 @@ echo "Hello, dear $(whoami)!" > info.txt
 echo "date: $(date)" >> info.txt
 echo "OS: $(uname)" >> info.txt
 echo "home directory: $(pwd)" >> info.txt
-used=$(df -h | awk 'NR==2{print $4}')
-free=$(df -h | awk 'NR==2{print $5}')
+used=$(df $HOME -h | awk 'NR==2{print $4}')
+free=$(df $HOME -h | awk 'NR==2{print $5}')
 echo "used memory = $used" >> info.txt
 echo "free memory = $free" >> info.txt
-dir=$(ls -la | grep ^d | wc |awk '{print$1}')
+dir=$(ls $HOME -la | grep ^d | wc |awk '{print$1}')
 echo "folders = $dir" >> info.txt
-files=$(ls -laR | grep ^- | wc |awk '{print$1}')
-echo "files = $files" >> info.txt
+#files=$(ls $HOME -laR | grep ^- | wc |awk '{print$1}')
+echo "files = $(find $HOME -maxdepth 1 -type f | wc -l)" >> info.txt
