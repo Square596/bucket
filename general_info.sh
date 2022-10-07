81fbd48372a29e500c7f84871becf050b5ee5958
Square596 <pshenitsyn.as@phystech.edu>
2022-10-08 01:47:13 +0300

    v2

---
+++ general_info.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+path=$HOME/info.txt
+echo "Hello, dear $(whoami)!" > $path
+echo "date: $(date)" >> $path
+echo "OS: $(uname)" >> $path
+echo "home directory: $HOME" >> $path
+
+used=$(df $HOME -h | awk "NR==2{print $4}")
+free=$(df /c -h | awk "NR==2{print $5}")
+echo "used memory = $used" >> $path
+echo "free memory = $free" >> $path
+
+dir=$(ls $HOME -la | grep ^d | wc |awk '{print$1}')
+echo "folders = $dir" >> $path
+
+files=$(find $HOME -maxdepth 1 -type f | wc -l)
+echo "files = $files" >> $path
