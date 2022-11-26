35ec38b34472f529ec31411d4c06cca4ce33eb43
Square596 <pshenitsyn.as@phystech.edu>
2022-11-26 19:29:42 +0300

    v2

--- task8.py
+++ task8.py
@@ -1,17 +1,24 @@
 import random
 from typing import List, Optional
+from argparse import ArgumentParser
 
 
 class RandomChoiceIterator:
 
-    def __init__(self, values: List[int], num_iters: Optional[int] = None):
+    def __init__(self, values: List[int], num_iters: Optional[int] = None, seed: Optional[int|float] = None):
+        if not values:
+            raise ValueError('List of values is empty! It must contain at least one value!')
         self.values = values
         self.num_iters = num_iters
-        if self.num_iters:
+        random.seed(seed)
+        if self.num_iters is not None:
             self.current_iteration = 0
 
+    def __iter__(self):
+        return self
+
     def __next__(self):
-        if not self.num_iters:
+        if self.num_iters is None:
             return random.choice(self.values)
         elif self.current_iteration >= self.num_iters:
             raise StopIteration()
@@ -19,8 +26,13 @@ class RandomChoiceIterator:
             self.current_iteration += 1
             return random.choice(self.values)
 
-    def __iter__(self):
-        return self
 
-for value in RandomChoiceIterator([1, 2, 3], num_iters=5):
-    print(value)
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('-v', '--values', required=True, nargs='*', type=int)
+    parser.add_argument('-n', '--num-iters', required=False, type=int, default=None)
+    parser.add_argument('-s', '--seed', required=False, type=float, default=None)
+    args = parser.parse_args()
+
+    for value in RandomChoiceIterator(values=args.values, num_iters=args.num_iters, seed=args.seed):
+        print(value)
