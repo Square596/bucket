d480e2979ea2e048f76cc21f7bd0e8509c6fd9c7
Square596 <pshenitsyn.as@phystech.edu>
2022-12-02 17:20:54 +0300

    v2

--- task1.py
+++ task1.py
@@ -1,9 +1,9 @@
 persons = input().split(', ')
 out = ''
 if len(persons) < 3:
-    raise NameError('Too few characters! - Please, enter at least 3 characters')
+    raise TypeError('Too few characters! - Please, enter at least 3 characters')
 if persons[-1] != 'лиса':
-    raise NameError("The last character must be 'лиса'!")
+    raise ValueError("The last character must be 'лиса'!")
 for person in range(len(persons)-2):
     out += 'от ' + persons[person] + ' ушёл, '
 out += 'и от ' + persons[-2] + ' ушёл, а лиса — ам его! и нету колобка...'
