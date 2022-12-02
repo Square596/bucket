abb5819294358503e57beb5582c208413c5502e4
Square596 <pshenitsyn.as@phystech.edu>
2022-12-02 20:30:05 +0300

    v1

--- task11_2.py
+++ task11_2.py
@@ -4,20 +4,21 @@ import os
 
 
 def search(path, name, string):
+    output = ''
     with open(os.path.join(path, name), 'r') as file:
         while True:
             line = file.readline()
             if not line:
-                break
+                return output
             if string in line:
-                print(f'{name}: {line[:-2]}')
+                output += f'{name}: {line}'
 
 
-def separate_txt_iterable(path):
+def separate_txt_iterable(path, string):
     for file in os.listdir(path):
         if os.path.isfile(os.path.join(path, file)):
             if os.path.splitext(file)[1] == '.txt':
-                yield file
+                yield path, file, string
 
 
 if __name__ == '__main__':
@@ -26,6 +27,5 @@ if __name__ == '__main__':
     parser.add_argument('path', type=str)
     args = parser.parse_args()
 
-    pool = Pool(os.cpu_count()-1)
-    for x in separate_txt_iterable(args.path):
-        pool.apply(search, args=(args.path, x, args.string))
+    pool = Pool()
+    print(''.join(pool.starmap(search, separate_txt_iterable(args.path, args.string))))
